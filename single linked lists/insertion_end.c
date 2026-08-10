2450f793fd1f6d8949429598351c933973dbdbf4
BabarZargar <babarzargar95@gmail.com>
2026-08-10 18:21:04 +0530

    added code for inserting at end in a linked list

---
+++ single linked lists/insertion_end.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+void add_at_end(struct node *head, int data){
+    struct node *ptr, *temp;
+    ptr = head;
+    temp = (struct node *)malloc(sizeof(struct node));
+
+    temp -> data = data;
+    temp -> link = NULL;
+
+    while(ptr -> link != NULL){
+        ptr = ptr -> link;
+    }
+    ptr -> link = temp;
+
+}
+
+int main(){
+    struct node *head = malloc(sizeof(struct node));
+    head -> data = 45;
+    head -> link = NULL;
+
+    struct node *current = malloc(sizeof(struct node));
+    current -> data = 98;
+    current -> link = NULL;
+    head -> link = current;
+
+    current = malloc(sizeof(struct node));
+    current -> data = 75;
+    current -> link = NULL;
+    head -> link -> link = current;
+
+    add_at_end(head, 67);
+
+    printf("%d", head -> link -> link -> link -> data);
+
+    return 0;
+
+}
