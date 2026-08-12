7b124f38486af12b92e2076f9e2f568f380ed73f
BabarZargar <babarzargar95@gmail.com>
2026-08-12 20:39:07 +0530

    added code for inserting node at any position

--- single linked lists/insertion_pos.c
+++ single linked lists/insertion_pos.c
@@ -6,6 +6,53 @@ struct node{
     struct node *link;
 };
 
+void add_at_end(struct node *head, int data){
+    struct node *ptr, *temp;
+    ptr = head;
+    temp = (struct node *)malloc(sizeof(struct node));
+
+    temp -> data = data;
+    temp -> link = NULL;
+
+    while(ptr -> link != NULL){
+        ptr = ptr -> link;
+    }
+    ptr -> link = temp;
+
+}
+void add_at_pos(struct node *head, int data, int pos){
+    struct node *ptr = head;
+    struct node *ptr2 = malloc(sizeof(struct node));
+
+    ptr2 -> data = data;
+    ptr2 -> link = NULL;
+
+    pos--;
+    while(pos!=1){
+        ptr = ptr -> link;
+        pos--;
+    }
+    ptr2 -> link = ptr-> link;
+    ptr -> link = ptr2;
+}
+
 int main(){
+    struct node *head = malloc(sizeof(struct node));
+    head -> data = 27;
+    head -> link = NULL;
+
+    add_at_end(head, 29);
+    add_at_end(head, 67);
+
+    int data = 69, pos = 3;
+    add_at_pos(head, data, pos);
+    struct node *ptr = head;
+
+    while(ptr!=NULL){
+        printf("%d\n", ptr -> data);
+        ptr = ptr -> link;
+    }
+
+    return 0;
 
 }
